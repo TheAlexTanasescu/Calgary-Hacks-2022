b3281a3b87291dc440b7c2fbb75f4eaee6a6a1e0
Sherriff Kadiri <sherriff.kadiri@ucalgary.ca>
2022-02-19 13:32:19 -0700

    Habit and Group
    Updated the Habit class to allow descriptions and have a level.
    
    Created a Group class that allows an individual to group Habits together.

--- hackoverflow/main/Habit.java
+++ hackoverflow/main/Habit.java
@@ -1,17 +1,42 @@
 package hackoverflow.main;
 public class Habit {
-	String text;
+	int level;
+	String title;
+	String desc;
 	Frequency freq;
-	Habit(String inText, Frequency inFreq) {
-		this.text = inText;
-		this.freq = inFreq;
+	
+	Habit(String inTitle, String inDesc, Frequency inFreq) {
+		setHabitTitle(inTitle);
+		setHabitFrequency(inFreq);
+		setHabitDesc(inDesc);
+		setHabitLevel(0);
 	}
 	
-	String getHabitText() {
-		return this.text;
+	String getHabitTitle() {
+		return this.title;
+	}
+	
+	String getHabitDesc() {
+		return this.desc;
 	}
 	
 	Frequency getHabitFrequency() {
 		return this.freq;
+	}
+	
+	void setHabitTitle(String inTitle) {
+		this.title = new String(inTitle);
+	}
+	
+	void setHabitDesc(String inDesc) {
+		this.desc = new String(inDesc);
+	}
+	
+	void setHabitFrequency(Frequency inFreq) {
+		this.freq = inFreq;
+	}
+	
+	void setHabitLevel (int inLevel) {
+		this.level = inLevel;
 	}
 }
