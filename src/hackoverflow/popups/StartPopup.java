4b2607fdec8ffc67c09006e6feb6da3c6977c0f7
TheAlexTanasescu <alskiboy@yahoo.com>
2022-02-20 03:07:57 -0700

    startPopup

--- src/hackoverflow/popups/StartPopup.java
+++ src/hackoverflow/popups/StartPopup.java
@@ -1,6 +1,9 @@
 package hackoverflow.popups;
 
+import java.awt.Image;
+
 import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JRadioButton;
@@ -10,13 +13,37 @@ public class StartPopup {
 	
 	private static JOptionPane startPopup;
 	
-	public static void main()
+	public StartPopup()
 	{
 		JLabel intro = new JLabel("Let's begin with your name. What is it?");
 		JTextField name = new JTextField();
-		JRadioButton checkBox1 = new JRadioButton("Justin Biever");   
-        JRadioButton checkBox2 = new JRadioButton("Big Mac");  
-        JRadioButton checkBox3 = new JRadioButton("Firequacker");  
+		JLabel choice = new JLabel("I have four habpets from you to choose from. Which one do you pick?");
+		
+		//JLabel first = new JLabel("Justin Beaver");
+		ImageIcon petIcon = new ImageIcon("res/pet_justin0.png");
+		Image img = petIcon.getImage();
+		Image newimg = img.getScaledInstance(100, 100,  java.awt.Image.SCALE_SMOOTH);
+		ImageIcon newIcon = new ImageIcon(newimg);
+		JLabel first = new JLabel(newIcon);
+		
+		JRadioButton checkBox1 = new JRadioButton("Justin Beaver");
+		 petIcon = new ImageIcon("res/pet_moomask0.png");
+		 img = petIcon.getImage();
+		 newimg = img.getScaledInstance(100, 100,  java.awt.Image.SCALE_SMOOTH);
+		 newIcon = new ImageIcon(newimg);
+		 JLabel second = new JLabel(newIcon);
+        JRadioButton checkBox2 = new JRadioButton("Big Mac"); 
+        petIcon = new ImageIcon("res/pet_friedchicken0.png");
+		 img = petIcon.getImage();
+		 newimg = img.getScaledInstance(100, 100,  java.awt.Image.SCALE_SMOOTH);
+		 newIcon = new ImageIcon(newimg);
+		 JLabel third = new JLabel(newIcon);
+        JRadioButton checkBox3 = new JRadioButton("Firequacker");
+        petIcon = new ImageIcon("res/pet_wiggly0.png");
+		 img = petIcon.getImage();
+		 newimg = img.getScaledInstance(100, 100,  java.awt.Image.SCALE_SMOOTH);
+		 newIcon = new ImageIcon(newimg);
+		 JLabel fourth = new JLabel(newIcon);
         JRadioButton checkBox4 = new JRadioButton("Sushi"); 
         checkBox1.setBounds(100,100, 50,50); 
         checkBox2.setBounds(100,150, 50,50);  
@@ -28,11 +55,14 @@ public class StartPopup {
         bg.add(checkBox3);
         bg.add(checkBox4);
 		Object[] messageArr = {
-			"Intro", intro,
+			intro,
 		    "Name", name,
-		    "Habpet:", checkBox1, checkBox2, checkBox3, checkBox4
+		    choice,
+		    "Habpet:\n", checkBox1,first, checkBox2, second, checkBox3, third, checkBox4, fourth
 		    
 		};
+		
+		int option = JOptionPane.showOptionDialog(null, messageArr, "Add New Habit", 0, JOptionPane.INFORMATION_MESSAGE, null, null, null);
 	}
 
 }
