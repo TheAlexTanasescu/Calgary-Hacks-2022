60ee2bc1de2aaa015a5c27dc0e7a0d6906a80128
Ramez <ramezhalasah@gmail.com>
2022-02-19 21:19:17 -0700

    editing pp

--- src/hackoverflow/pages/ProfilePage.java
+++ src/hackoverflow/pages/ProfilePage.java
@@ -2,19 +2,27 @@ package hackoverflow.pages;
 
 import java.awt.Button;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Frame;
+import java.awt.Graphics;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Label;
+import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 import hackoverflow.main.Main;
 
 
 public class ProfilePage extends PagePanel {
+	
+	
+	
 
 	public ProfilePage(PageFrame frame)
 	 
@@ -30,18 +38,18 @@ public class ProfilePage extends PagePanel {
 		    });
 	        setSize(300,300);        //setting size.
 	        setName("StudyTonight");  //setting title.
-	        setLayout(new GridBagLayout());	 //set default layout for frame.
+	        setLayout(new GridBagLayout()); //set default layout for frame.
+	        JLabel label = new JLabel("Profile");
+	        
+	        this.add(label);
+	        
+	        
 	        setVisible(true);           //set frame visibilty true.
 
-	        this.add(btn);
-			ProfilePage p = this;
-			btn.addActionListener(new ActionListener() {  
-		    	public void actionPerformed(ActionEvent e) {
-		    		frame.remove(p);
-		    		frame.add(new HabitsPage(null));
-		    		frame.validate();
-		        }  
-		    }); 
+	       
+			
+			
+		    
 	
 
 	     	 
