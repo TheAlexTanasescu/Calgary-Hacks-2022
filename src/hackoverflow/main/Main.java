7fbe8a1d274b29af3965613371acdb0e80050e1b
Ramez <ramezhalasah@gmail.com>
2022-02-19 18:53:39 -0700

    changed test function

--- src/hackoverflow/main/Main.java
+++ src/hackoverflow/main/Main.java
@@ -6,6 +6,7 @@ import javax.swing.JFrame;
 import hackoverflow.pages.HabitsPage;
 import hackoverflow.pages.PageFrame;
 import hackoverflow.pages.PagePanel;
+import hackoverflow.pages.PageProfile;
 
 
 public class Main {
@@ -18,14 +19,19 @@ public class Main {
     public static void main(String[] args) {
 	    PageFrame startFrame = new PageFrame(width, height, title);
 	 	PagePanel startPanel = new PagePanel(startFrame);
+	 	PagePanel habitPanel = new HabitsPage(startFrame);
+	 	PagePanel profilePage = new PageProfile(startFrame);
 	 	startFrame.add(startPanel);
 	 	
-	 	test(startFrame, startPanel);
+	 	test(startFrame, startPanel,habitPanel);
+	 	
     }
     
-    private static void test(PageFrame frame, PagePanel panel) {
+    private static void test(PageFrame frame, PagePanel panel, PagePanel panel2) {
     	frame.remove(panel);
-    	frame.add(new HabitsPage(frame));
+    	frame.add(panel2);
     	frame.validate();
     }
+    
+    
 }
