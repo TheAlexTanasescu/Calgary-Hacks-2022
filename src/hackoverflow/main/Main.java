d2f9afa69e24608d793db69f19201c77a4aa4d87
Ramez <ramezhalasah@gmail.com>
2022-02-19 19:32:53 -0700

    segsi

--- src/hackoverflow/main/Main.java
+++ src/hackoverflow/main/Main.java
@@ -20,35 +20,23 @@ public class Main {
     public static void main(String[] args) {
     	// Frame
 	    PageFrame startFrame = new PageFrame(width, height, title);
-<<<<<<< HEAD
-	 	PagePanel startPanel = new PagePanel(startFrame);
+
+	 //	PagePanel startPanel = new PagePanel(startFrame);
 	 	PagePanel habitPanel = new HabitsPage(startFrame);
 	 	PagePanel profilePage = new PageProfile(startFrame);
-	 	startFrame.add(startPanel);
-	 	
-	 	test(startFrame, startPanel,habitPanel);
-	 	
-    }
-    
-    private static void test(PageFrame frame, PagePanel panel, PagePanel panel2) {
-=======
-
-	    //Panel
-	 	PagePanel startPanel = new PagePanel();
-	 	startFrame.add(new HabitsPage(startFrame));
+	 	startFrame.add(habitPanel);
 	 	startFrame.validate();
 	 	
-	 	//Layout
-	 	PageLayout layout = new PageLayout();
-	 	layout.setGrid(startPanel);
 	 	
     }
     
+    
+    
     private static void switchToHabitsPage(PageFrame frame, PagePanel panel) {
->>>>>>> 3091032b018356da347a69480b3bd9d0cb758355
-    	frame.remove(panel);
-    	frame.add(panel2);
-    	frame.validate();
+
+    	//frame.remove(panel);
+    	frame.add(panel);
+    	
     }
     
     
--- src/hackoverflow/pages/HabitsPage.java
+++ src/hackoverflow/pages/HabitsPage.java
@@ -1,16 +1,9 @@
 package hackoverflow.pages;
 
-import java.awt.BorderLayout;
-import java.awt.Button;
 import java.awt.Color;
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
-
 import java.awt.*;  
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.GridBagConstraints;
 import java.awt.event.ActionEvent;
@@ -18,20 +11,33 @@ import java.awt.event.ActionListener;
 
 public class HabitsPage extends PagePanel {
 	private static final long serialVersionUID = 25974988494620193L;
+
+	//buttons here
+	
 	
 	public HabitsPage(JFrame frame) {
+
 		this.setBackground(Color.LIGHT_GRAY);
 		initButtons(frame);
 	}
-	
+	private static void test(PageFrame frame, PagePanel panel, PagePanel panel2) {
+    	frame.remove(panel);
+    	frame.add(panel2);
+    	frame.validate();
+    }
 
 	public void initButtons(JFrame frame) {
+		GridBagConstraints gb = new GridBagConstraints();
+		this.setLayout(new GridBagLayout());
+		this.setVisible(true);
+		
 		Button b = new Button("P");
 		this.add(b);
 		b.addActionListener(new ActionListener() {  
 	    	public void actionPerformed(ActionEvent e) {
 	    		frame.dispose();
 	    		System.out.print("Welcome to Javatpoint.");
+	    		
 	        }  
 	    });
 		
@@ -45,6 +51,11 @@ public class HabitsPage extends PagePanel {
 	}
 	
 	public void addHabit() {
+		GridBagConstraints gb = new GridBagConstraints();
+		this.setLayout(new GridBagLayout());
+		this.setVisible(true);
+		
+		
 		Button addHabit = new Button("Add Habit");
 		this.add(addHabit);
 		addHabit.addActionListener(new ActionListener() {
